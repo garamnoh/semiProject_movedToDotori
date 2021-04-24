54960c4d6cac727eefc26adf06c8599a3c3861d1
garam noh <garam.nohh@gmail.com>
2021-04-24 17:59:03 +0900

    1. 1200px로 가운데 만들어 놓으니까 너무 이상해서 일단 크기는 화면에 꽉채워서 만들었습니다. 미니홈피 적용되는것 보고 다시 조절하겠습니다. 2. 아이콘은 홈버튼부터 좌측 4개는 작동하구요 사이드 메뉴는 iframe으로 연결해 놨습니다. (미니홈피 아이콘은 뭘 좋아하실지 몰라서 그냥 몇 개 만들어 봤어요) 3. 친구관리와 쇼핑 화면 사이드 바 디자인은 다르게 해놨는데 쇼핑쪽 사이드바는 버튼 누르면 펼쳐지는 메뉴 입니다. 디자인은 작업하면서 얘기해주시는대로 변경하겠습니다.

---
+++ js/main.js
@@ -0,0 +1,74 @@
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+// 선언
+let logo = document.querySelector('.bar>img:first-child');
+
+let homeBtn = document.getElementById('homeBtn');
+let groupBtn = document.getElementById('groupBtn');
+let shopBtn = document.getElementById('shopBtn');
+let miniBtn = document.getElementById('miniBtn');
+
+let sidemenu = document.querySelector('body>.main>.mainContainer>.sidemenu');
+
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+// 기능
+
+// 홈버튼 클릭이벤트
+homeBtn.addEventListener('click', (e)=>{
+    // 선택된 버튼 아이콘 색상 진하게
+    $('.bar>img').not(e.target).not($(logo)).addClass('focused');
+    (e.target).classList.remove('focused');
+
+    sidemenu.classList.remove('hide');
+    // 사이드바에 있는 아이프레임 삭제
+    let removeIframe = document.querySelector('body>.main>.mainContainer>.sidemenu>iframe');
+    sidemenu.removeChild(removeIframe);
+    // 버튼에 따른 아이프레임 메뉴 생성
+    let iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'sidebarHome.html')
+    sidemenu.appendChild(iframe);
+});
+
+// 그룹버튼 클릭이벤트
+groupBtn.addEventListener('click', (e)=>{
+    // 선택된 버튼 아이콘 색상 진하게
+    $('.bar>img').not(e.target).not($(logo)).addClass('focused');
+    (e.target).classList.remove('focused');
+
+    sidemenu.classList.remove('hide');
+    // 사이드바에 있는 아이프레임 삭제
+    let removeIframe = document.querySelector('body>.main>.mainContainer>.sidemenu>iframe');
+    sidemenu.removeChild(removeIframe);
+    // 버튼에 따른 아이프레임 메뉴 생성
+    let iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'sidebarGroup.html');
+    sidemenu.appendChild(iframe);
+});
+
+// 쇼핑버튼 클릭이벤트
+shopBtn.addEventListener('click', (e)=>{
+    // 선택된 버튼 아이콘 색상 진하게
+    $('.bar>img').not(e.target).not($(logo)).addClass('focused');
+    (e.target).classList.remove('focused');
+
+    sidemenu.classList.remove('hide');
+    // 사이드바에 있는 아이프레임 삭제
+    let removeIframe = document.querySelector('body>.main>.mainContainer>.sidemenu>iframe');
+    sidemenu.removeChild(removeIframe);
+    // 버튼에 따른 아이프레임 메뉴 생성
+    let iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'sidebarShop.html');
+    sidemenu.appendChild(iframe);
+});
+
+// 미니홈피버튼 클릭이벤트
+miniBtn.addEventListener('click', (e)=>{
+    // 선택된 버튼 아이콘 색상 진하게
+    $('.bar>img').not(e.target).not($(logo)).addClass('focused');
+    (e.target).classList.remove('focused');
+    // 사이드바 숨기기(display:none)
+    sidemenu.classList.add('hide');
+});
